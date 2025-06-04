b32f497807ccef850cdbf2245f62c7ade5e38ccd
jannis.guski@scai.fraunhofer.de <jannis.guski@scai.fraunhofer.de>
2025-06-04 14:40:51 +0000

    Feat: started with TriNetX integration

--- src/cohort_specific/factory.py
+++ src/cohort_specific/factory.py
@@ -1,6 +1,8 @@
 from .ukbiobank.merge_covariates_ukb import merge_covariates_ukbiobank
+from .trinetx.merge_covariates_tnx import merge_covariates_trinetx
 
 COHORT_SPECIFIC_COV_MERGE_FUNCTIONS = {
     "mock_data": lambda df, **kwargs: df,  # don't merge anything for the mock dataset
     "ukbiobank": merge_covariates_ukbiobank,
+    "trinetx": merge_covariates_trinetx
 }
